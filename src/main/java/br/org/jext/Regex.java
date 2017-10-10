ae063450b9b2068069aeb07bd03cdc7ef073c824
Thiago Alexandre Martins Monteiro <thiago.amm.agr@gmail.com>
2017-10-10 11:48:06 -0300

    1 - Adicionadas novas expressões regulares para telefone fixo, telefone celular, chassi e placa de veículo.

--- src/main/java/br/org/jext/Regex.java
+++ src/main/java/br/org/jext/Regex.java
@@ -9,6 +9,11 @@ public enum Regex {
    
    CPF("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}"),
    CNPJ("\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}"),
+   REGEX_CHASSI_VEICULO("[A-Z]{3}\\.[A-Z0-9]{6}\\.[A-Z0-9]{2}\\.[A-Z0-9]{6}"),
+   REGEX_PLACA_VEICULO("[A-Z]{3}\\-\\d{4}"),
+   TELEFONE_FIXO("\\(\\d{2}\\) \\d{4}-\\d{4}"),
+   TELEFONE_CELULAR("\\(\\d{2}\\) \\d{5}-\\d{4}"),
+   EMAIL("^[a-z]+[a-z0-9._-]+@(\\w+\\.\\w+)+$"),
    SQL_DATETIME_FORMAT("\\d{4}-\\d{1,2}-\\d{1,2} \\d{1,2}:\\d{1,2}:\\d{1,2}"), 
    SQL_DATE_FORMAT("\\d{4}-\\d{1,2}-\\d{1,2}"), 
    SQL_TIME_FORMAT("\\d{1,2}:\\d{1,2}:\\d{1,2}");
